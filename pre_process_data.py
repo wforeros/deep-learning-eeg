9afe1bcdfd90031404d7351f4258ca616b16077c
Wilson Forero <wilson_camilo@hotmail.com>
2019-12-06 01:03:38 -0500

    Realiza la normalización de un set de datos almacenado en la ruta especificada Hace la separación de train y test en dos carpetas dentro del directorio especificado

--- pre_process_data.py
+++ pre_process_data.py
@@ -5,31 +5,36 @@
 """
 __author__ = 'Wilson Forero'
 
-import utilities
+import core
 import os.path as op
 import os
 
+#sets_package='Set3-0ms'
+sets_package = core.SETS_PACKAGES[2]
 #%%
 # =============================================================================
 # En esta sección se realiza la normalización de datos
 # =============================================================================
-#sets_package='Set3-0ms'
-sets_package = utilities.SETS_PACKAGES[1]
 
 
-for sub_cat in utilities.SUB_CATEGORIES:
+
+for sub_cat in core.SUB_CATEGORIES:
     category = 'Emoticon' if 'E' in sub_cat else 'Real'
-    directory_origin = op.join(utilities.CLASSIFIED_SETS_FOLDER, sets_package, category, sub_cat)
-    mne_files = utilities.get_files_with_mne(directory_origin)
+    directory_origin = op.join(core.CLASSIFIED_SETS_FOLDER, sets_package, category, sub_cat)
+    mne_files = core.get_files_with_mne(directory_origin)
     output_folder = op.join(
-                utilities.PROCESSED_WITH_PYTHON_FOLDER, sets_package, category, sub_cat
+                core.PROCESSED_WITH_PYTHON_FOLDER, sets_package, category, sub_cat
             )
-    utilities.create_directory(output_folder)
+    core.create_directory(output_folder)
+    i = 0
     for file in mne_files:
+        # Los canales Fp1 y Fp2 fueron usados para remover EOG por Gratton
+        file.drop_channels(['Fp1', 'Fp2'])
         original_data = file.get_data()
-        normalized_data = utilities.normalize_array(original_data)
-        file_name = utilities.get_mne_filename(file)
-        normalized_data = utilities.normalize_array(original_data)
+        normalized_data = core.normalize_array(original_data, mode='epochs')
+#        print('Ya normalizado', normalized_data.shape, 'iter', i)
+        file_name = core.get_mne_filename(file)
+        normalized_data = core.normalize_array(original_data)
         file._data = normalized_data
         file_path = op.join(output_folder, '{}{}'.format(file_name, '-epo.fif'))
         file.save(file_path, verbose=1)
@@ -40,15 +45,14 @@ for sub_cat in utilities.SUB_CATEGORIES:
 # =============================================================================
         
 test_size = 20.#%
-sets_package='Set2-100ms'
 
 
-for sub_cat in utilities.SUB_CATEGORIES:
+for sub_cat in core.SUB_CATEGORIES:
     category = 'Emoticon' if 'E' in sub_cat else 'Real'
-    origin_folder = op.join(utilities.PROCESSED_WITH_PYTHON_FOLDER, sets_package, category, sub_cat)
+    origin_folder = op.join(core.PROCESSED_WITH_PYTHON_FOLDER, sets_package, category, sub_cat)
     all_files = os.listdir(path=origin_folder)
     output_folder = op.join(
-                utilities.PROCESSED_AND_CLASSIFIED_FOLDER, sets_package, category, sub_cat
+                core.PROCESSED_AND_CLASSIFIED_FOLDER, sets_package, category, sub_cat
             )
-    utilities.create_directory(output_folder)
-    utilities.split_sets(origin_folder, output_folder)
+    core.create_directory(output_folder)
+    core.split_sets(origin_folder, output_folder)
